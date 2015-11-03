643ac40b6056503fbd7fc6b15dc818a04d300c6a
dich1 <daich125912@gmail.com>
2015-11-03 13:27:02 +0000

    add top page

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   root to: 'static_pages#home'
 end
