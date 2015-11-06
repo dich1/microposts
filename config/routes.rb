35859ddccf503f002a2adfbaa43b89220c386d2e
dich1 <daich125912@gmail.com>
2015-11-06 09:35:16 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
   root to: 'static_pages#home'
+  get 'signup',  to: 'users#new'
+
+  resources :users
 end
--- db/migrate/20151103143052_create_users.rb
+++ db/migrate/20151103143052_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
 
       t.timestamps null: false
+      
       t.index :email, unique: true # この行を追加
     end
   end
