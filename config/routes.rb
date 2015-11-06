d30d6db2b047e4677aacddec0c08b8076f49c8ee
dich1 <daich125912@gmail.com>
2015-11-06 10:32:35 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
-
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
