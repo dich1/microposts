ad4a0bf71d25331267e9cc2460c410adafaec214
dich1 <daich125912@gmail.com>
2015-11-07 15:23:57 +0000

    ユーザーのプロフィール更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
   
   def show
    @user = User.find(params[:id])
@@ -18,10 +19,35 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to user_path , notice: 'メッセージを編集しました' # リンク飛ばす部分
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :area, :email, :password,
                                  :password_confirmation)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103143052) do
+ActiveRecord::Schema.define(version: 20151107142000) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151103143052) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
