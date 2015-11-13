5523f698855a09ae158d320e9498a5b87c973d7d
dich1 <daich125912@gmail.com>
2015-11-13 13:19:05 +0000

    add home view's timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
+  end
 end
